8a3cd7d497d7618d5a6f7e1b6f922bfe680e5b66
zxjcarrot <zxjcarrot@gmail.com>
2015-10-15 18:41:17 +0800

    修改2.3题

--- chapter2/2.3.cpp
+++ chapter2/2.3.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main () {
-    cout << (int)'a' << ' '
-         << (int)'e' << ' '
-         << (int)'i' << ' '
-         << (int)'o' << ' '
-         << (int)'u'
+    cout << "(int)'a'=" << (int)'a' << ' '
+         << "(int)'e'=" << (int)'e' << ' '
+         << "(int)'i'=" << (int)'i' << ' '
+         << "(int)'o'=" << (int)'o' << ' '
+         << "(int)'u'=" << (int)'u'
          << endl;
     return 0;
 }
